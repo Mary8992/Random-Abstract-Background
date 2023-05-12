e345f8d3bf38dbdf852d5229c300c22f8024f865
Mary8992 <mc046569@gmail.com>
2023-05-12 23:15:07 +0300

    generating block functionality and elements added

--- app.js
+++ app.js
@@ -6,3 +6,20 @@ for (let i = 0; i <= 80; i++) {
     blocks.classList.add('block');
     container.appendChild(blocks);
 }
+
+function generate() {
+    anime({
+        targets : '.block',
+        translateX : function () {
+            return anime.random(-700, 700);
+        },
+        translateY : function() {
+            return anime.random(-500, 500);
+        },
+        scale : function() {
+            return anime.random(1,5);
+        }
+    })
+}
+
+generate();
